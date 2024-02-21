0cbc52230cde603b07a72effa51d8c1d4399108c
Gabriel Silva Torres <gabrielst328@gmail.com>
2024-02-20 23:16:08 -0300

    Extension methods

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ExtensionMethods.Extensions;
+
+namespace ExtensionMethods;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        DateTime dt = new DateTime(2024, 02, 20, 8, 10, 45);
+        Console.WriteLine(dt.ElapsedTime());
+    } 
+}
